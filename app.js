271b7e76d61c5d87fce554f0d4c7bbcf6d2b6548
sebelousov <bsn@list.ru>
2021-03-29 21:17:55 +0300

    add getting content from textarea

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let button = document.querySelector('button')
+let resultTour = document.getElementById('resultTour')
+let resultGamer = document.getElementById('resultGamer')
+
 button.addEventListener('click', () => {
-    console.log('button')
+    console.log(resultTour.value
+                          .split('\n'))
+    console.log(resultGamer.value)
 })
 
-function shell(resultTour, inputData, result) {
-    let inputElement = document.getElementById(input).value;
-    let elementOut = document.getElementById(result);
-    elementOut.innerHTML = inputElement;
-}
+// function shell(resultTour, inputData, result) {
+//     let inputElement = document.getElementById(input).value;
+//     let elementOut = document.getElementById(result);
+//     elementOut.innerHTML = inputElement;
+// }
