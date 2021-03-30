2ea809ea26de32d517254a78218a1c624bf63bc5
sebelousov <bsn@list.ru>
2021-03-30 22:06:02 +0300

    refresh app.js

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ let resultTour = document.getElementById('resultTour')
 let resultGamer = document.getElementById('resultGamer')
 
 button.addEventListener('click', () => {
-    console.log(resultTour.value
-                          .split('\n'))
-    console.log(resultGamer.value)
+    let regexp = /[а-яА-Я]+/
+    let strings = resultTour.value
+                            .split('\n')
+                            .map((s) => {
+                                return s.substring(s.match(regexp).index)
+                            })
+    for (let iterator of strings) {
+        console.log(getGameResult(iterator))
+    }
+    //console.log(resultGamer.value)
 })
+
+function getGameResult(string) {
+    let regExp = /[^0-9А-Яа-я]+/
+    return string.split(regExp)
+                 .filter((e) => {
+                  console.log(e)
+                  return e.length > 1 && /[а-яА-Я]+/.test(e)
+    })
+}
 
 // function shell(resultTour, inputData, result) {
 //     let inputElement = document.getElementById(input).value;
