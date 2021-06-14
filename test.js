a2db5d8b5b9c642bcb2875a734dfa0b337eaad50
sebelousov <bsn@list.ru>
2021-06-14 19:09:18 +0300

    testing js

--- test.js
+++ test.js
@@ -87,8 +87,67 @@
 // console.log(topSalary({}))
 // console.log(topSalary(salaries))
 
-let date = new Date(Date.parse('2012-02-20T03:12Z+03:00'))
-let now = new Date()
+// let date = new Date(Date.parse('2012-02-20T03:12Z+03:00'))
+// let now = new Date()
 
-console.log(date)
-console.log(now)
+// console.log(date)
+// console.log(now)
+
+// async function* generateSequence(start, end) {
+//     for (let i = start; i <= end; i++) {
+//         await new Promise(resolve => setTimeout(resolve, 1000))
+
+//         yield i
+//     }
+// }
+
+// (async () => {
+//     for await (let value of generateSequence(1, 4)) {
+//         console.log(value)
+//     }
+// })()
+
+// let range = {
+//     from: 2, 
+//     to: 4, 
+//     *[Symbol.iterator]() {
+//         for (let value = this.from; value <= this.to; value++) {
+//             yield value
+//         }
+//     }
+// }
+
+// console.log(Array.from(range))
+
+async function* fetchCommits(repo) {
+    let url = `https://api.github.com/repos/${repo}/commits`
+
+    while (url) {
+        const response = await fetch(url, {
+            headers: {'User-Agent': 'Our-script'}
+        })
+
+        const body = await response.json()
+
+        let nextPage = response.headers.get('Link').match(/<(.*?)>; rel="next"/)
+        nextPage = nextPage && nextPage[1]
+
+        url = nextPage
+
+        for (let commit of body) {
+            yield commit
+        }
+    }
+}
+
+(async () => {
+    let count = 0
+
+    for await (const commit of fetchCommits('sebelousov/advego')) {
+        console.log(commit.author.login)
+
+        if (++count == 100) {
+            break
+        }
+    }
+})()
